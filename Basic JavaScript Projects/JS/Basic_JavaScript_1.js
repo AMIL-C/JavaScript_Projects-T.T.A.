740b8dcfc5e2d0a46cd53ef92ad382aa55164587
Amil S. Boddie-Willis <abwst12@users.noreply.github.com>
2020-04-27 14:12:33 -0500

    Just did some basic code practicing using JavaScript.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+/* window.alert("Today already looks like it might be a challenging day."); */
+
+/* document.write("Monday is always my least favorite day of the week."); */
+
+/* document.write("\"Sometimes it\'s best just to put a task aside for a bit, so as to give yourself time to slowdown and calm your mind.<br> " + " Ultimately, it\'s just good for the brain.\""); */
+
+/* var fruits = "Favorite Fruits", Number_One_Favorite = "Mangos", Second_Favorite = "Apples", Third_Favorite = "Kiwis", Fourth_Favorite = "Oranges";
+document.write(Number_One_Favorite); */
+
+/* var red = "I've been seeing nothing but red everywhere I look.";
+var red = red.fontcolor("red");
+
+document.write(red); */
+
+document.write(10 + 10);
+
