ba4f94d7b07ac939d50cb615ab838504a3db1161
Amil S. Boddie-Willis <abwst12@users.noreply.github.com>
2020-04-28 16:52:58 -0500

    I just practiced some new JavaScript skills that I learned.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function Fruits_And_Vegetables() {
+    var fruits_vegetables = "Organically grown produce is just one of several ways nature helps<br> to keep living a long, healthier life.";
+    var result = fruits_vegetables.fontcolor("orange");
+    document.getElementById("orange_fruits_vegetables").innerHTML = result;
+}
+
+/* function Read_Watch_Imagination() {
+    var creative_juices_flowing = "Books and movies";
+    creative_juices_flowing += " really help to fire up my imagination<br> and get my creative juices flowing!!";
+    document.getElementById("imagination_on_fire").innerHTML = creative_juices_flowing;
+} */
+
